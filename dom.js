aecf3a947b0149f39a246ec720e06d79a9558c04
FIRE-BLAST <ankitprakash074@gmail.com>
2023-10-06 19:01:17 +0530

    domdone

---
+++ dom.js
@@ -0,0 +1,26 @@
+//console.dir(document);
+//console.log(document.domain);
+
+//var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent = 'hello';
+//headerTitle.innerText = 'goodbye';
+header.style.borderBottom = 'solid 3px #000';
+
+
+//var items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//items[1].textContent = 'hello 2';
+//items[1].style.fontWeight = 'bold';
+//items[1].style.backgroundColor = 'yellow';
+
+//var itm = document.getElementsByClassName('card card-body');
+
+
+//for(var i = 0; i < items.length; i++){
+ //   items[i].style.backgroundcolor = 'red';
+//}
+
+var header = document.querySelector('container');
+header.style.fontWeight = 'bold';
